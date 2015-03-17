efa12a75438ce8981b557e6443f48ca364fe41c7
John <cabaj@wisc.edu>
2015-03-17 00:37:36 -0500

    Corrected stitching for positive shift in y

--- src/main.m
+++ src/main.m
@@ -1,7 +1,7 @@
 function main()
 
-    srcImagesFolder = '..\..\testingImages\'; 
-    srcImagesFiles = dir(strcat(srcImagesFolder, '*_panorama.jpg'));
+    srcImagesFolder = '..\..\department\'; 
+    srcImagesFiles = dir(strcat(srcImagesFolder, '*.jpg'));
     imageNames = {srcImagesFiles.name};
     numImages = numel(imageNames);
     image = imread(strcat(srcImagesFolder, srcImagesFiles(1).name));
@@ -12,47 +12,58 @@ function main()
         images(:,:,:,i) = imread(strcat(srcImagesFolder, srcImagesFiles(i).name));
     end
 
-    cylImages = cylinderProjection(images, numImages, 595);
+    cylImages = cylinderProjection(images, numImages, 682.05069);
     
     offsets = zeros(1,2);
+    x_ranges = ones(1,2);
+    y_ranges = ones(1,2);
     x_offset_total = 0;
     y_offset_total = 0;
+    up_growth = 0;
+    down_growth = 0;
     
-    for i = 1 : numImages
-        if(i < numImages)
-            H = match(cylImages(:,:,:,i+1), cylImages(:,:,:,i), 4, 500);
-            offsets(i,:) = [H(1,3), H(2,3)];
-        else
-            H = match(cylImages(:,:,:,1), cylImages(:,:,:,i), 4, 500);
-            offsets(i,:) = [H(1,3), H(2,3)];
-        end
+    for i = 1 : numImages - 1
+    	H = match(cylImages(:,:,:,i), cylImages(:,:,:,i+1), 4, 1000);
+        offsets(i+1,:) = [H(1,3), H(2,3)];
+%         else
+%             H = match(cylImages(:,:,:,i), cylImages(:,:,:,1), 4, 1000);
+%             offsets(i,:) = [H(1,3), H(2,3)];
+%         end
         
-        x_offset_total = x_offset_total + offsets(i,1);
-        y_offset_total = x_offset_total + offsets(i,2);
+        x_ranges(i,2) = x_offset_total + size(image, 2) + round(offsets(i+1,1));
+        x_ranges(i+1,1) = x_ranges(i,2) + 1;
+        x_offset_total = x_ranges(i,2);
+        y_offset_total = y_offset_total + abs(round(offsets(i+1,2)));
+        if(round(offsets(i+1,2)) > 0)
+           down_growth = down_growth + abs(round(offsets(i+1,2)));
+        else
+           up_growth = up_growth + abs(round(offsets(i+1,2)));
+        end
+    end
+
+    x_offset_total = x_offset_total + size(image,2);
+    y_offset_total = y_offset_total + size(image,1);
+    x_ranges(size(x_ranges,1),2) = x_offset_total;
+    panorama = uint8(zeros(y_offset_total,x_offset_total,3));
+    
+    y_ranges(1,1) = up_growth + 1;
+    y_ranges(1,2) = y_ranges(1,1) + size(image,1) - 1;
+    
+    for i = 2 : size(offsets,1)
+       y_ranges(i,1) = y_ranges(i-1,1) + round(offsets(i,2)); 
+       y_ranges(i,2) = y_ranges(i,1) + size(image,1) - 1; 
     end
     
-    x_offset = floor(H(1,3));
-    y_offset = floor(H(2,3));
-    new_image = uint8(zeros(size(image,1)+y_offset_total,2*size(image,2)+x_offset_total,3));
-%     image2 = cylImages(:,:,:,5);
-%     image1 = cylImages(:,:,:,4);
-%     x_limit = size(image,2) + x_offset;
-%     y_limit = size(image,1) + y_offset;
-%     
-%    	for x = 1 : size(new_image,2)
-%        for y = 1 : size(new_image,1)
-%            if(x > x_limit)
-%                 if(y > y_offset)
-%                     new_image(y,x,:) = image1(y-y_offset,x-x_limit,:);
-%                 end
-%            else
-%                if(y <= size(image2,1))
-%                     new_image(y,x,:) = image2(y,x,:);
-%                end
-%            end
-%        end
-%     end
+    for i = 1 : numImages
+        image = cylImages(:,:,:,i);
+        
+        for x = x_ranges(i,1) : x_ranges(i,2)
+           for y = y_ranges(i,1) : y_ranges(i,2)
+                panorama(y,x,:) = image(y - y_ranges(i,1) + 1,x - x_ranges(i,1) + 1,:);
+           end
+        end
+    end
     
-    figure, imshow(new_image);
+    figure, imshow(panorama);
 end
 
